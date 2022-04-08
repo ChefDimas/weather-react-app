a8d6b2433a3779c32beda0a4358325ce88bdf1d7
ChefDimas <dimalunik@aol.com>
2022-04-08 11:41:29 +0300

    reset

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import "./App.sass";
-import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="weather-app">
-        <div className="container">
-          <h3 className="brand"></h3>
-          <div>
-            <h1 className="temp"> </h1>
-            <div className="city-time">
-              <h1 className="name"></h1>
-              <small>
-                <span className="date"></span>
-              </small>
-            </div>
-            <div className="weather">
-              <img
-                src={require("./assets/icons/sunny.png")}
-                alt="sunny"
-                className="icon"
-                width="50"
-                height="50"
-              />
-              <span className="condition">Sunny</span>
-            </div>
-          </div>
-        </div>
-        <div className="panel">
-          <form id="locationInput">
-            <input
-              type="text"
-              className="search"
-              placeholder="Search Location..."
-            />
-            <button type="submit" className="submit"></button>
-          </form>
-        </div>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
