3228650edb7c09a6071865eb155dbccb5518614f
ChefDimas <dimalunik@aol.com>
2022-07-04 18:21:28 +0300

    Create const with api

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import axios from "axios";
 
 function App() {
 
-
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat={44.439663}&lon={26.096306}&appid={dd6bade7424688cbb50162ce860ff70b}";
 
   return(
-  <div></div>
+  <div>{console.log(url)}</div>
   );
 }
 
