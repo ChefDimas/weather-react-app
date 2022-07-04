0cfeeb17b4097447653a2728f77311ff814cd21f
ChefDimas <dimalunik@aol.com>
2022-07-04 18:37:30 +0300

    Fix api key

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import axios from "axios";
 
 function App() {
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?lat={44.439663}&lon={26.096306}&appid={dd6bade7424688cbb50162ce860ff70b}";
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.43923&lon=26.1063&appid=dd6bade7424688cbb50162ce860ff70b";
 
   return(
-  <div>{console.log(url)}</div>
+  <div></div>
   );
 }
 
